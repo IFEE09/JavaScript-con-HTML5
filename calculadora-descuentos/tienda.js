6536085dc8060c277526d276dd09db1f1faafda7
IFEE09 <ivanfaridespadas@gmail.com>
2021-11-27 16:28:41 -0600

    checando el sweetalert

--- calculadora-descuentos/tienda.js
+++ calculadora-descuentos/tienda.js
@@ -1,3 +1,4 @@
+import Swal from 'sweetalert2';
 
 //funcion que calcula un precio con descuento
 function precioConDescuento(precio, descuento) {
@@ -7,4 +8,15 @@ function precioConDescuento(precio, descuento) {
 //interactuando con el html -------------------------------------------------------------------------
 function obtenerDescuento() {
     
+    const input_Precio = document.getElementById('precio');
+    const value_Precio = Number(input_Precio.value);
+
+    const input_Descuento = document.getElementById('descuento');
+    const value_Descuento = Number(input_Descuento.value);
+    
+    if(value_Descuento < 0 || value_Descuento > 100) {
+        
+
+    }
+
 }
