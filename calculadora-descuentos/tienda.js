8665b661bcd6e91e15800b7856174e26a6958028
IFEE09 <ivanfaridespadas@gmail.com>
2021-11-27 15:50:07 -0600

    agregando calculadora de descuentos

---
+++ calculadora-descuentos/tienda.js
@@ -0,0 +1,10 @@
+
+//funcion que calcula un precio con descuento
+function precioConDescuento(precio, descuento) {
+    return ((100-descuento)/100) * precio;
+}
+
+//interactuando con el html -------------------------------------------------------------------------
+function obtenerDescuento() {
+    
+}
