f67b9769fafd3df00c93da27b23c5e1ea810a2e0
IFEE09 <ivanfaridespadas@gmail.com>
2021-11-28 08:29:08 -0600

    agregando sweetalert2 en calculadora-descuentos

--- calculadora-descuentos/tienda.js
+++ calculadora-descuentos/tienda.js
@@ -1,5 +1,3 @@
-import Swal from 'sweetalert2';
-
 //funcion que calcula un precio con descuento
 function precioConDescuento(precio, descuento) {
     return ((100-descuento)/100) * precio;
@@ -9,14 +7,40 @@ function precioConDescuento(precio, descuento) {
 function obtenerDescuento() {
     
     const input_Precio = document.getElementById('precio');
-    const value_Precio = Number(input_Precio.value);
+    let value_Precio = input_Precio.value;
 
     const input_Descuento = document.getElementById('descuento');
-    const value_Descuento = Number(input_Descuento.value);
+    let value_Descuento = input_Descuento.value;
     
-    if(value_Descuento < 0 || value_Descuento > 100) {
-        
+    //validacion para saber si los campos estan vacios
+    if(value_Precio === '' || value_Descuento === ''){
+        Swal.fire({
+            icon: 'error',
+            title: 'Los campos no deben de estar vacios'
+        });
+
+    } else {
+
+        value_Descuento = Number(value_Descuento);
+        value_Precio = Number(value_Precio);
+
+        if(value_Descuento < 0 || value_Descuento > 100) {
+            Swal.fire({
+                icon: 'error',
+                title: 'El descuento debe de estar entre 0 y 100'
+            });
+    
+        } else {
+            const value_PrecioConDescuento = precioConDescuento(value_Precio, value_Descuento);
+            Swal.fire({
+                icon: 'succes',
+                title: `El precio con descuento es ${value_PrecioConDescuento}`
+            });
+        }
 
     }
+
+
+    
 
 }
